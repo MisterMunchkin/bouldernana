daac6efd595153c2ec5302f2bb960ff78e353e8c
MisterMunchkin <9417970+MisterMunchkin@users.noreply.github.com>
2025-02-09 19:31:59 +1000

    feat: :sparkles: allowed links and notes to be updatable

---
+++ app/climb-log/[id]/update-notes.tsx
@@ -0,0 +1,61 @@
+import { addClimbSchema } from "@/constants/zod-schema.const";
+import { router, useLocalSearchParams } from "expo-router";
+import { ClimbLogLocalParams } from ".";
+import { useUserClimbRecordStore } from "@/stores/user-climb-record.store";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { ScrollView, View } from "react-native";
+import { cn } from "@/utils/cn.util";
+import { TextField } from "@/components/core/field";
+import PressableOpacity from "@/components/core/pressable-opacity";
+import AppText from "@/components/core/app-text";
+
+const schema = addClimbSchema.pick({
+    notes: true,
+});
+
+type Props = {};
+
+const UpdateNotes = ({}: Props) => {
+    const { id } = useLocalSearchParams<ClimbLogLocalParams>();
+    const climbLog = useUserClimbRecordStore((store) => store.getLog(id));
+    const updateLog = useUserClimbRecordStore((store) => store.updateClimb);
+
+    const form = useForm({
+        resolver: zodResolver(schema),
+        defaultValues: schema.parse(climbLog),
+    });
+    const { handleSubmit, control } = form;
+
+    const updateRecord = (update: z.infer<typeof schema>) => {
+        updateLog(id, update);
+        router.back();
+    };
+
+    return (
+        <ScrollView className={cn("flex-1")}>
+            <View className="pt-safe-offset-4 gap-6 px-4">
+                <FormProvider {...form}>
+                    <TextField
+                        control={control}
+                        name="notes"
+                        title="Notes of the climb."
+                        className="h-32"
+                        inputProps={{ multiline: true, numberOfLines: 5 }}
+                    />
+
+                    <PressableOpacity
+                        onPress={handleSubmit(updateRecord)}
+                        color={"submit"}
+                        rounded={"lg"}
+                    >
+                        <AppText size={"xs"}>Submit</AppText>
+                    </PressableOpacity>
+                </FormProvider>
+            </View>
+        </ScrollView>
+    );
+};
+
+export default UpdateNotes;
