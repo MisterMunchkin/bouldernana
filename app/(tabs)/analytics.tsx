222ebca994595132f9c084ea4137a0841d8d858a
MisterMunchkin <9417970+MisterMunchkin@users.noreply.github.com>
2025-02-09 20:27:53 +1000

    feat: :children_crossing: analytics coming soon

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -6,8 +6,27 @@ type Props = {};
 const analytics = ({}: Props) => {
     return (
         <ScrollView className="flex-1">
-            <View className="py-safe-offset-20">
-                <AppText>Analytics</AppText>
+            <View className="py-safe-offset-20 items-center gap-6">
+                <AppText>Coming Soon!</AppText>
+                <AppText size={"xs"}>
+                    Some features I'm thinking of including:
+                </AppText>
+                <View className="flex-col items-start gap-4 px-4">
+                    <AppText size={"xs"}>
+                        - Number of climbs logged this month
+                    </AppText>
+                    <AppText size={"xs"}>
+                        - Average grade of the climbs logged this month
+                    </AppText>
+                    <AppText size={"xs"}>
+                        - Radar charts of data like, steepness, skills, type of
+                        ascents
+                    </AppText>
+                    <AppText size={"xs"}>
+                        - Correlation between the grades you sent, how it felt,
+                        if you project it or flash it
+                    </AppText>
+                </View>
             </View>
         </ScrollView>
     );
