78b5d4d7b859793ce26a6de1c9a120a0650b98d3
MisterMunchkin <robindalmy@gmail.com>
2025-02-16 09:11:51 +1000

    feat: :sparkles: organized the profile screen and also added export as json button
    '

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,80 +1,24 @@
-import DropdownField from "@/components/core/dropdown-field";
-import {
-    BoulderGradeSystemEnum,
-    RouteGradeSystemEnum,
-    settingsSchema,
-} from "@/constants/zod-schema.const";
-import { useUserSettingsStore } from "@/stores/user-settings.store";
-import { zodResolver } from "@hookform/resolvers/zod";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { ClassValue } from "clsx";
-import { useEffect } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import AppText from "@/components/core/app-text";
+import PressableOpacity from "@/components/core/pressable-opacity";
+import GradeSystemPreferences from "@/components/profile/grade-system-preferences";
 import { View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { z } from "zod";
 
 type Props = {};
 
 const profile = ({}: Props) => {
-    const boulderSettings = useUserSettingsStore(
-        (store) => store.boulderSettings
-    );
-    const routeSettings = useUserSettingsStore((store) => store.routeSettings);
-    const updateSettings = useUserSettingsStore(
-        (store) => store.updateSettings
-    );
-
-    const form = useForm({
-        resolver: zodResolver(settingsSchema),
-        defaultValues: settingsSchema.parse({
-            boulderSettings,
-            routeSettings,
-        }),
-    });
-
-    const { control, watch, handleSubmit } = form;
-
-    const onSubmit = (values: z.infer<typeof settingsSchema>) =>
-        updateSettings(values);
-
-    useEffect(() => {
-        const watchSubscribe = watch(() => handleSubmit(onSubmit)());
-        return () => watchSubscribe.unsubscribe();
-    }, [watch, handleSubmit]);
-
-    const boulderGradeOptions = BoulderGradeSystemEnum.options;
-    const routeGradeOptions = RouteGradeSystemEnum.options;
-    const selected: ClassValue = "bg-red-500";
     return (
         <KeyboardAwareScrollView className="px-4">
             <View className="gap-8 flex-grow py-safe-offset-20">
-                <FormProvider {...form}>
-                    <DropdownField
-                        control={control}
-                        name="boulderSettings.gradeSystem"
-                        title="Boulder Grading System"
-                        items={boulderGradeOptions.map((value) => ({
-                            label: value,
-                            value,
-                        }))}
-                        classNames={{
-                            selected,
-                        }}
-                    />
-                    <DropdownField
-                        control={control}
-                        name="routeSettings.gradeSystem"
-                        title="Route Grading System"
-                        items={routeGradeOptions.map((value) => ({
-                            label: value,
-                            value,
-                        }))}
-                        classNames={{
-                            selected,
-                        }}
-                    />
-                </FormProvider>
+                <GradeSystemPreferences />
+                <View className="gap-4">
+                    <PressableOpacity color={"submit"} rounded={"lg"}>
+                        <AppText size={"xs"}>Export as JSON</AppText>
+                    </PressableOpacity>
+                    <AppText align={"center"} size={"xxs"} twClassName="italic">
+                        What is JSON?
+                    </AppText>
+                </View>
             </View>
         </KeyboardAwareScrollView>
     );
---
+++ components/profile/grade-system-preferences.tsx
@@ -0,0 +1,75 @@
+import {
+    BoulderGradeSystemEnum,
+    RouteGradeSystemEnum,
+    settingsSchema,
+} from "@/constants/zod-schema.const";
+import { useUserSettingsStore } from "@/stores/user-settings.store";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
+import DropdownField from "@/components/core/dropdown-field";
+import { ClassValue } from "clsx";
+
+type Props = {};
+
+const GradeSystemPreferences = ({}: Props) => {
+    const boulderSettings = useUserSettingsStore(
+        (store) => store.boulderSettings
+    );
+    const routeSettings = useUserSettingsStore((store) => store.routeSettings);
+    const updateSettings = useUserSettingsStore(
+        (store) => store.updateSettings
+    );
+    const form = useForm({
+        resolver: zodResolver(settingsSchema),
+        defaultValues: settingsSchema.parse({
+            boulderSettings,
+            routeSettings,
+        }),
+    });
+
+    const { control, watch, handleSubmit } = form;
+
+    const onSubmit = (values: z.infer<typeof settingsSchema>) =>
+        updateSettings(values);
+
+    useEffect(() => {
+        const watchSubscribe = watch(() => handleSubmit(onSubmit)());
+        return () => watchSubscribe.unsubscribe();
+    }, [watch, handleSubmit]);
+
+    const boulderGradeOptions = BoulderGradeSystemEnum.options;
+    const routeGradeOptions = RouteGradeSystemEnum.options;
+    const selected: ClassValue = "bg-red-500";
+    return (
+        <FormProvider {...form}>
+            <DropdownField
+                control={control}
+                name="boulderSettings.gradeSystem"
+                title="Boulder Grading System"
+                items={boulderGradeOptions.map((value) => ({
+                    label: value,
+                    value,
+                }))}
+                classNames={{
+                    selected,
+                }}
+            />
+            <DropdownField
+                control={control}
+                name="routeSettings.gradeSystem"
+                title="Route Grading System"
+                items={routeGradeOptions.map((value) => ({
+                    label: value,
+                    value,
+                }))}
+                classNames={{
+                    selected,
+                }}
+            />
+        </FormProvider>
+    );
+};
+
+export default GradeSystemPreferences;
