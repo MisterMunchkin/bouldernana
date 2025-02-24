a56749e686b09285abd279fa140af44c3b8486fa
MisterMunchkin <robindalmy@gmail.com>
2025-02-24 17:48:43 +1000

    feat: :sparkles: added loader for profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,20 @@
 import AppText from "@/components/core/app-text";
 import PressableOpacity from "@/components/core/pressable-opacity";
 import GradeSystemPreferences from "@/components/profile/grade-system-preferences";
-import { settingsSchema } from "@/constants/zod-schema.const";
+import ProfileLoader from "@/components/profile/profile-loader";
+import Loader from "@/components/ui/loader";
 import { useUserClimbRecordStore } from "@/stores/user-climb-record.store";
 import { useUserSettingsStore } from "@/stores/user-settings.store";
 import { FileSystemUtil } from "@/utils/file-system.util";
+import { useCallback, useState } from "react";
 import { View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type Props = {};
 
 const profile = ({}: Props) => {
+    const [isImporting, setIsImporting] = useState<boolean>(false);
+
     const climbLogs = useUserClimbRecordStore((store) => store.climbs);
     const restoreClimbRecord = useUserClimbRecordStore(
         (store) => store.flashRecordFromJSON
@@ -20,8 +24,12 @@ const profile = ({}: Props) => {
         useUserSettingsStore((store) => store);
 
     const handleImport = async () => {
+        setIsImporting((prev) => !prev);
         const result = await FileSystemUtil.importJSON();
-        if (!result) return;
+        if (!result) {
+            setIsImporting((prev) => !prev);
+            return;
+        }
         const { boulderSettings, routeSettings, climbLogs } = result;
 
         restoreClimbRecord({ climbLogs });
@@ -29,42 +37,49 @@ const profile = ({}: Props) => {
             boulderSettings,
             routeSettings,
         });
+        setIsImporting((prev) => !prev);
     };
 
     return (
         <KeyboardAwareScrollView className="px-4">
             <View className="gap-8 flex-grow py-safe-offset-20">
-                <GradeSystemPreferences
-                    defaultValues={{
-                        boulderSettings,
-                        routeSettings,
-                    }}
-                    onSubmit={updateSettings}
-                />
-                <View className="gap-4">
-                    <PressableOpacity
-                        color={"submit"}
-                        rounded={"lg"}
-                        onPress={() =>
-                            FileSystemUtil.saveJSON({
-                                data: JSON.stringify({
-                                    climbLogs,
-                                    boulderSettings,
-                                    routeSettings,
-                                }),
-                            })
-                        }
-                    >
-                        <AppText size={"xs"}>Export as JSON</AppText>
-                    </PressableOpacity>
-                    <PressableOpacity
-                        twClassName="bg-core-caribbean-current-600"
-                        rounded={"lg"}
-                        onPress={handleImport}
-                    >
-                        <AppText size={"xs"}>Import as JSON</AppText>
-                    </PressableOpacity>
-                </View>
+                {isImporting ? (
+                    <ProfileLoader />
+                ) : (
+                    <>
+                        <GradeSystemPreferences
+                            defaultValues={{
+                                boulderSettings,
+                                routeSettings,
+                            }}
+                            onSubmit={updateSettings}
+                        />
+                        <View className="gap-4">
+                            <PressableOpacity
+                                color={"submit"}
+                                rounded={"lg"}
+                                onPress={() =>
+                                    FileSystemUtil.saveJSON({
+                                        data: JSON.stringify({
+                                            climbLogs,
+                                            boulderSettings,
+                                            routeSettings,
+                                        }),
+                                    })
+                                }
+                            >
+                                <AppText size={"xs"}>Export as JSON</AppText>
+                            </PressableOpacity>
+                            <PressableOpacity
+                                twClassName="bg-core-caribbean-current-600"
+                                rounded={"lg"}
+                                onPress={handleImport}
+                            >
+                                <AppText size={"xs"}>Import as JSON</AppText>
+                            </PressableOpacity>
+                        </View>
+                    </>
+                )}
             </View>
         </KeyboardAwareScrollView>
     );
