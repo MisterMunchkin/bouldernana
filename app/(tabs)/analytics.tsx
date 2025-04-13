7db6d7e40ecec48bc52259fc3c71c4536ac04871
MisterMunchkin <robindalmy@gmail.com>
2025-04-13 16:30:40 +1000

    feat: :sparkles: using new chart kit for analytics

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -1,35 +1,84 @@
+import { Analytics } from "@/classes/climb-analytic.class";
 import AppText from "@/components/core/app-text";
-import { ScrollView, View } from "react-native";
+import { useUserClimbRecordStore } from "@/stores/user-climb-record.store";
+import { Dimensions, ScrollView, View } from "react-native";
+
+import { LineChart, PieChart } from "react-native-chart-kit";
 
 type Props = {};
 
+const screenWidth = 400;
+const chartHeight = 250;
+const chartConfig = {
+	backgroundGradientFrom: "#1E2923",
+	backgroundGradientFromOpacity: 0,
+	backgroundGradientTo: "#08130D",
+	backgroundGradientToOpacity: 0.5,
+	color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+	strokeWidth: 2, // optional, default 3
+	barPercentage: 0.5,
+	useShadowColorFromDataset: false, // optional
+};
+
 const analytics = ({}: Props) => {
-    return (
-        <ScrollView className="flex-1">
-            <View className="py-safe-offset-20 items-center gap-6">
-                <AppText>Coming Soon!</AppText>
-                <AppText size={"xs"}>
-                    Some features I'm thinking of including:
-                </AppText>
-                <View className="flex-col items-start gap-4 px-4">
-                    <AppText size={"xs"}>
-                        - Number of climbs logged this month
-                    </AppText>
-                    <AppText size={"xs"}>
-                        - Average grade of the climbs logged this month
-                    </AppText>
-                    <AppText size={"xs"}>
-                        - Radar charts of data like, steepness, skills, type of
-                        ascents
-                    </AppText>
-                    <AppText size={"xs"}>
-                        - Correlation between the grades you sent, how it felt,
-                        if you project it or flash it
-                    </AppText>
-                </View>
-            </View>
-        </ScrollView>
-    );
+	const climbs = useUserClimbRecordStore((store) => store.climbs);
+	// const totalTypesOfClimbs = new ClimbAnalytic(
+	// 	climbs
+	// ).getTotalTypesOfClimbs();
+	const typeOfCLimbAnalytic = new Analytics({
+		data: climbs,
+	});
+
+	return (
+		<ScrollView className="flex-1">
+			<View className="py-safe-offset-20 items-center gap-6">
+				<AppText>Coming Soon!</AppText>
+				<AppText size={"xs"}>
+					Some features I'm thinking of including:
+				</AppText>
+				{/* <View className="flex-col items-start gap-4 px-4">
+					<AppText size={"xs"}>
+						- Number of climbs logged this month
+					</AppText>
+					<AppText size={"xs"}>
+						- Average grade of the climbs logged this month
+					</AppText>
+					<AppText size={"xs"}>
+						- Radar charts of data like, steepness, skills, type of
+						ascents
+					</AppText>
+					<AppText size={"xs"}>
+						- Correlation between the grades you sent, how it felt,
+						if you project it or flash it
+					</AppText>
+				</View> */}
+				{/* <LineChart
+					data={{
+						labels: [],
+						datasets: [
+							{
+								data: [],
+							},
+						],
+					}}
+					width={screenWidth}
+					height={chartHeight}
+					verticalLabelRotation={30}
+					chartConfig={chartConfig}
+					bezier
+				/> */}
+				<PieChart
+					accessor="value"
+					data={typeOfCLimbAnalytic.toPieChartData("typeOfClimb")}
+					width={screenWidth}
+					height={chartHeight}
+					backgroundColor="transparent"
+					paddingLeft="15"
+					chartConfig={chartConfig}
+				/>
+			</View>
+		</ScrollView>
+	);
 };
 
 export default analytics;
--- constants/zod-schema.const.ts
+++ constants/zod-schema.const.ts
@@ -1,34 +1,34 @@
 import { z } from "zod";
 import {
-    ASCENT_TYPE,
-    CLIMB_FEEL,
-    FRENCH_GRADES,
-    SKILL_TYPE,
-    STEEPNESS,
-    VGRADES,
-    WHERE,
+	ASCENT_TYPE,
+	CLIMB_FEEL,
+	FRENCH_GRADES,
+	SKILL_TYPE,
+	STEEPNESS,
+	VGRADES,
+	WHERE,
 } from "./core.const";
 
 export const ClimbTypeEnum = z.enum(["Board", "Boulder", "Route", "Trad"]);
 
 export const addClimbSchema = z.object({
-    videoAssetIds: z.array(z.string()).optional(),
-    typeOfClimb: ClimbTypeEnum.default("Boulder"), //boulder, route, board, trad
-    whereDidYouClimb: z.enum(WHERE).default("Indoor"), //indoor, outdoor
-    grade: z.string(),
-    //Advanced info
-    ascentType: z.enum(ASCENT_TYPE).optional(), //redpoint, onsight, flash, project
-    attempts: z.coerce.number().optional(), //Should only show up if ascentType is redpoint or project
-    hasBeenSent: z.boolean(),
-    howDidItFeel: z.enum(CLIMB_FEEL).optional(), //soft, solid, hard
-    skill: z.enum(SKILL_TYPE).array().optional(), //Cruxy, Athletic, etc..
-    steepness: z.enum(STEEPNESS).optional(), // Slab, Overhang, etc.
-    //ENDOF Advanced info
-    // rating: z.string().optional(), // no rating to 5
-    date: z.string().datetime(),
-    notes: z.string().optional(),
-    link: z.string().optional(),
-    // relativeEffort: z.string().optional(), //strava effort input
+	videoAssetIds: z.array(z.string()).optional(),
+	typeOfClimb: ClimbTypeEnum.default("Boulder"), //boulder, route, board, trad
+	whereDidYouClimb: z.enum(WHERE).default("Indoor"), //indoor, outdoor
+	grade: z.string(),
+	//Advanced info
+	ascentType: z.enum(ASCENT_TYPE).optional(), //redpoint, onsight, flash, project
+	attempts: z.coerce.number().optional(), //Should only show up if ascentType is redpoint or project
+	hasBeenSent: z.boolean(),
+	howDidItFeel: z.enum(CLIMB_FEEL).optional(), //soft, solid, hard
+	skill: z.enum(SKILL_TYPE).array().optional(), //Cruxy, Athletic, etc..
+	steepness: z.enum(STEEPNESS).optional(), // Slab, Overhang, etc.
+	//ENDOF Advanced info
+	// rating: z.string().optional(), // no rating to 5
+	date: z.string().datetime(),
+	notes: z.string().optional(),
+	link: z.string().optional(),
+	// relativeEffort: z.string().optional(), //strava effort input
 });
 
 const SupportedRouteGrades = ["YDS", "FRENCH"] as const;
@@ -37,15 +37,15 @@ export const RouteGradeSystemEnum = z.enum(SupportedRouteGrades);
 export const BoulderGradeSystemEnum = z.enum(SupportedBoulderGrades);
 
 export const settingsSchema = z.object({
-    routeSettings: z.object({
-        gradeSystem: RouteGradeSystemEnum.default("FRENCH"),
-    }),
-    boulderSettings: z.object({
-        gradeSystem: BoulderGradeSystemEnum.default("VSCALE"),
-    }),
+	routeSettings: z.object({
+		gradeSystem: RouteGradeSystemEnum.default("FRENCH"),
+	}),
+	boulderSettings: z.object({
+		gradeSystem: BoulderGradeSystemEnum.default("VSCALE"),
+	}),
 });
 
 export const jsonExportSchema = z.object({
-    climbLogs: z.array(z.object({ ...addClimbSchema.shape, id: z.string() })),
-    ...settingsSchema.shape,
+	climbLogs: z.array(z.object({ ...addClimbSchema.shape, id: z.string() })),
+	...settingsSchema.shape,
 });
